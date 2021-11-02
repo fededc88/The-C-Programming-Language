5f3c34986c216b5343dc281f11a00bcf222ed722
Federico Ceccarelli <fededc88@gmail.com>
2021-11-02 01:33:12 -0300

    Chapter1 initial commit & added Exercise 1-1 with 'hello, world'. You must start from the beginning.

---
+++ Chapter1/1.1/src/1-1.c
@@ -0,0 +1,15 @@
+/*
+ * Excerside 1-1. Run the "hello, world" program on your system.
+ *
+ * Writen by Federico D. Ceccarelli (fededc88@gmail.com), any kinrdg of submision are welcome. 
+ */
+
+#include <stdio.h>
+
+void main(void) {
+    printf("hello, world\n");
+}
+
+//
+// End of file.
+//
