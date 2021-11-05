1a09b9cca5b73f7dd3b4c3893b5e8832503edaf4
Federico Ceccarelli <fededc88@gmail.com>
2021-11-05 20:01:10 -0300

    eexercise_1-7.c

---
+++ Chapter1/exercise_1-7.c
@@ -0,0 +1,26 @@
+/*
+ * Exercise 1-7. Write a program to print the value of EOF.
+ * 
+ * Writen by Federico D. Ceccarelli (fededc88@gmail.com), any kinrdg of submision are welcome. 
+ */
+  
+#include <stdio.h>
+
+void main (void)
+{
+ 
+    int c;
+/*
+ * NOTE: The value of the EOF character is -1
+ * 
+ * Shortcut to access EOF:
+ *     Windows: CTRL+Z
+ *     Unix like systems: CTRL+D.
+ */
+            printf("EOF: %d \n\r", EOF);
+}
+
+// 
+// End of file.
+//
+
