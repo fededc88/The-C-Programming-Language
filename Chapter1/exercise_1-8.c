efd0ab951de01acacf1eecd1a3a92c1b8e5b56c9
Federico Ceccarelli <fededc88@gmail.com>
2021-11-05 20:46:33 -0300

    e18.c

---
+++ Chapter1/exercise_1-8.c
@@ -0,0 +1,43 @@
+
+/*
+ * Exercise 1-8. Write a program to count blanks, tabs, and newlines.
+ * 
+ * Writen by Federico D. Ceccarelli (fededc88@gmail.com), any kinrdg of
+ * submision are welcome. 
+ */
+  
+#include <stdio.h>
+
+void main (void)
+{
+ 
+    int c;
+    long n1, n2, n3;
+
+    n1 = 0;
+    n2 = 0;
+    n3 = 0;
+
+/*
+ * NOTE: Counts blank, tabs and new lines characters until you type EOF.
+ * 
+ * Shortcut to access EOF:
+ *     Windows: CTRL+Z
+ *     Unix like systems: CTRL+D.
+ */
+    while ((c = getchar()) != EOF) {
+
+        if ( c == ' ')
+            ++n1;
+        if ( c == '\t')
+            ++n2;
+        if ( c == '\n' )
+            ++n3;
+    }
+
+    printf("blanks: %ld, tabs: %ld, nl: %ld \n" , n1, n2, n3);
+}
+
+// 
+// End of file.
+//
