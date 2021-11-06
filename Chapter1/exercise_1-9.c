8a36780ce58515f81f6ddb3d62e7505004c68a93
Federico Ceccarelli <fededc88@gmail.com>
2021-11-05 21:07:37 -0300

    added e 1-9

---
+++ Chapter1/exercise_1-9.c
@@ -0,0 +1,35 @@
+
+
+/*
+ * Exercise 1-9. Write a program to copy its input to its putput, replacing each
+ * string of one or more blanks by a single blank.
+ * 
+ * Writen by Federico D. Ceccarelli (fededc88@gmail.com), any kinrdg of
+ * submision are welcome. 
+ */
+  
+#include <stdio.h>
+
+void main (void)
+{
+ 
+    int new_c, last_c;
+
+/*
+ * Shortcut to access EOF:
+ *     Windows: CTRL+Z
+ *     Unix like systems: CTRL+D.
+ */
+    while ((new_c = getchar()) != EOF) {
+
+        if ( last_c != ' ' )
+            putchar(new_c);
+
+        last_c = new_c;
+    }
+    return;
+}
+
+// 
+// End of file.
+//
