71713773956adeb80d7ea6e8c83ccd9511309554
Federico Ceccarelli <fededc88@gmail.com>
2021-12-01 00:41:07 -0300

    some error corrected, still have some work to do

--- Chapter2/exercise_2-1.c
+++ Chapter2/exercise_2-1.c
@@ -38,15 +38,58 @@ void compute_appropiate_values(void)
     char _char;
     printf("`char': ");
     for(_char = 1, acum = 1; _char > 0; _char*=2, acum*=2);
-    printf("(%d) to %lld\n", _char, acum - 1);
+    printf("(%d) to %lld\n", _char, (acum - 1));
 
     printf("`signed char': ");
-    printf("(%d) to %lld\n", _char, acum - 1);
+    printf("(%d) to %lld\n", _char, (acum - 1));
 
     unsigned char _uchar;
-    for(_char = 1, acum = 1; _uchar > 0; _uchar*=2, acum*=2);
     printf("`unsigned char': ");
-    printf("(%d) to %lld\n", 0, acum - 1);
+    for(_uchar = 1, acum = 1; _uchar > 0; _uchar*=2, acum*=2);
+    printf("(%d) to %lld\n", 0, (acum - 1));
+
+    printf("\n");
+
+    /* Integers */
+    short int _short_int;
+    printf("`signed short int': ");
+    for(_short_int = 1, acum = 1; _short_int > 0; _short_int*=2, acum*=2);
+    printf("(%d) to %lld\n", _short_int, acum - 1);
+
+    unsigned short int _ushort_int;
+    printf("`unsigned short int': ");
+    for(_ushort_int = 1, acum = 1; _ushort_int > 0; _ushort_int*=2, acum*=2);
+    printf("(%d) to %lld\n", 0, (acum - 1));
+
+    int _int;
+    printf("`signed int':  ");
+    for(_int = 1, acum = 1; _int > 0; _int*=2, acum*=2);
+    printf("(%d) to %lld\n", _int, acum - 1);
+
+    unsigned int _uint;
+    printf("`unsigned int': ");
+    for(_uint = 1, acum = 1; _uint > 0; _uint*=2, acum*=2);
+    printf("(%d) to %lld\n", 0, (acum -1));
+
+    long int _long_int;
+    printf("`signed long int': ");
+    for(_long_int = 1, acum = 1; _long_int > 0; _long_int*=2, acum*=2);
+    printf("(%ld) to %lld\n", _long_int, (acum - 1));
+
+    unsigned long int _ulong_int;
+    printf("`unsigned long int': ");
+    for(_ulong_int = 1, acum = 1; _ulong_int > 0; _ulong_int*=2, acum*=2);
+    printf("(%d) to %lld\n", 0, (acum - 1));
+
+    long long int _long_long_int;
+    printf("`signed long long int': ");
+    for(_long_long_int = 1, acum = 1; _long_long_int > 0; _long_long_int*=2, acum*=2);
+    printf("(%lld) to %lld\n", _long_long_int, (acum - 1));
+
+    unsigned long long int _ulong_long_int;
+    printf("`unsigned long long int': ");
+    for(_ulong_long_int = 1, acum = 1; _ulong_long_int > 0; _ulong_long_int*=2, acum*=2);
+    printf("(%d) to %lld\n", 0, (acum - 1));
 
 }
 
@@ -72,12 +115,12 @@ void print_appropiate_values_from_std_header(void)
     printf(" (%d) to %d (SHRT_MIN - SHRT_MAX) \n", SHRT_MIN, SHRT_MAX);
 
     printf("`unsigned short int': ");
-    printf("0 to %u (UINT_MIN - UINT_MAX) \n", SHRT_MAX);
+    printf("0 to %u (0 - USHRT_MAX) \n", USHRT_MAX);
 
     printf("`signed int':  ");
     printf("(%d) to %d (INT_MIN - INT_MAX)\n", INT_MIN, INT_MAX);
 
-    printf("`unsigned short int': ");
+    printf("`unsigned int': ");
     printf("0 to %u (0 - UINT_MAX)\n", UINT_MAX);
     
     printf("`signed long int': ");
